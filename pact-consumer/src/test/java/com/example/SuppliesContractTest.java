f0677d2feabeb1a8384d56a5df1bfd69d610482e
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2018-09-30 21:03:22 +0300

    Customer test for malformed date

--- pact-consumer/src/test/java/com/example/SuppliesContractTest.java
+++ pact-consumer/src/test/java/com/example/SuppliesContractTest.java
@@ -1,30 +1,28 @@
 package com.example;
 
 import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.model.RequestResponsePact;
+import org.apache.http.client.fluent.Request;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonArray;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(PactConsumerTestExt.class)
 @PactTestFor(providerName = "Supplies", port = "9051")
 public class SuppliesContractTest {
+    private static final String BASE_URL = "http://localhost:9051";
     private SuppliesClient client;
 
     @BeforeEach
     void setUp() {
-        client = new SuppliesClient("http://localhost:9051");
+        client = new SuppliesClient(BASE_URL);
     }
 
     @Pact(consumer="Planner")
@@ -50,11 +48,21 @@ public class SuppliesContractTest {
         client.getFor(LocalDate.of(2018, 12, 23));
     }
 
+    @Pact(consumer="Planner")
+    public RequestResponsePact malformedDate(PactDslWithProvider builder) {
+        return builder
+                .uponReceiving("request with malformed date")
+                .path("/supplies")
+                .query("day=abc")
+                .method("GET")
+                .willRespondWith()
+                .status(400)
+                .toPact();
+    }
+
     @Test
-    @PactTestFor(pactMethod = "oneSupply")
-    @Disabled
-    public void getsSupplies2() {
-        double counted = new SuppliesAnalyser().countAverageItemWeight(client.getFor(LocalDate.of(2018, 12, 23)));
-        assertThat(counted).isEqualTo(0.0d);
+    @PactTestFor(pactMethod = "malformedDate")
+    public void getWithMalformed() throws Exception {
+        Request.Get(BASE_URL + "/supplies?day=abc").execute();
     }
 }
